4854b3651899a7478bb8db27dacdbefcc6dc0c39
andyjojo <anguo.wenz@gmail.com>
2016-06-29 17:39:52 +0800

    for metrics
    Thank you for your solution to using an controller to export endpoint, I think add PublicMetricsAutoConfiguration can fix metrics only response {} problem, you can have a test

--- src/main/java/com/sample/springApp/config/AppConfig.java
+++ src/main/java/com/sample/springApp/config/AppConfig.java
@@ -3,6 +3,7 @@ package com.sample.springApp.config;
 import org.springframework.boot.actuate.autoconfigure.EndpointAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.HealthIndicatorAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.MetricRepositoryAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.PublicMetricsAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -10,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 //@EnableAutoConfiguration
-@Import({ EndpointAutoConfiguration.class, HealthIndicatorAutoConfiguration.class, MetricRepositoryAutoConfiguration.class})
+@Import({ EndpointAutoConfiguration.class, HealthIndicatorAutoConfiguration.class, PublicMetricsAutoConfiguration.class, MetricRepositoryAutoConfiguration.class})
 @ComponentScan("com.sample.springApp")
 @EnableWebMvc
 public class AppConfig {
